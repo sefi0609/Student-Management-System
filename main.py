8b6c88c7e2ccbd6ce80078b5fa9d061464a71918
Yosefi Kroytoro <81361291+sefi0609@users.noreply.github.com>
2023-11-29 02:50:55 +0200

    improving the search functionality and closing the db properly

--- main.py
+++ main.py
@@ -1,6 +1,127 @@
-from PyQt6.QtWidgets import QApplication
+from search_dialog import SearchDialog
+from PyQt6.QtWidgets import QApplication, QPushButton, QMainWindow, QTableWidget, QToolBar, QStatusBar, QTableWidgetItem
+from PyQt6.QtGui import QAction, QIcon
+from util import connect_to_db
+from about_dialog import AboutDialog
+from insert_dialog import InsertDialog
+from delete_dialog import DeleteDialog
+from edit_dialog import EditDialog
 import sys
-from main_window import MainWindow
+
+
+class MainWindow(QMainWindow):
+    """ Main window for the application with all the functionality """
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Student Management System')
+        self.setMinimumSize(505, 400)
+
+        # add menu items for the main window
+        file_menu = self.menuBar().addMenu('&File')
+        help_menu = self.menuBar().addMenu('&Help')
+        edit_menu = self.menuBar().addMenu('&Edit')
+
+        # create a widget
+        add_student = QAction(QIcon('icons/add.png'), 'Add student', self)
+        # connect the widget to a function, when the widget is clicked
+        add_student.triggered.connect(self.insert)
+        # add it to the menu bar
+        file_menu.addAction(add_student)
+
+        about_action = QAction('About', self)
+        about_action.triggered.connect(self.about)
+        help_menu.addAction(about_action)
+
+        search = QAction(QIcon('icons/search.png'), 'Search', self)
+        search.triggered.connect(self.search)
+        edit_menu.addAction(search)
+
+        # create a table widget
+        self.table = QTableWidget()
+        self.table.setColumnCount(5)
+        self.table.setHorizontalHeaderLabels(('Id', 'Name', 'Course', 'Mobile', 'Email'))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+        # create toolbar and add elements
+        toolbar = QToolBar()
+        toolbar.setMovable(True)
+        toolbar.addAction(add_student)
+        toolbar.addAction(search)
+        self.addToolBar(toolbar)
+
+        # create a status bar and add elements
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        # detect a cell click
+        self.table.cellClicked.connect(self.cell_clicked)
+
+    def load_data(self):
+        """ Load the students' data from the database """
+
+        conn = connect_to_db()
+        cursor = conn.cursor()
+        try:
+            cursor.execute('SELECT * FROM students')
+            result = cursor.fetchall()
+            self.table.setRowCount(0)
+            for row_num, row_data in enumerate(result):
+                self.table.insertRow(row_num)
+                for col_num, data in enumerate(row_data):
+                    self.table.setItem(row_num, col_num, QTableWidgetItem(str(data)))
+        except Exception as e:
+            print(f'Exception in MainWindow.load_data: {e}')
+            raise e
+        finally:
+            cursor.close()
+            conn.close()
+
+        # the status bar needs to be hidden
+        self.statusbar.setVisible(False)
+
+    def cell_clicked(self):
+        """ Show a status bar for every click on the table """
+
+        edit_button = QPushButton('Edit Record')
+        edit_button.clicked.connect(self.edit)
+
+        delete_button = QPushButton('Delete Record')
+        delete_button.clicked.connect(self.delete)
+
+        children = self.findChildren(QPushButton)
+        if children:
+            for child in children:
+                self.statusbar.removeWidget(child)
+
+        self.statusbar.addWidget(edit_button)
+        self.statusbar.addWidget(delete_button)
+
+        # the status bar needs to be visible
+        self.statusbar.setVisible(True)
+
+    def insert(self):
+        dialog = InsertDialog(self)
+        dialog.exec()
+
+    def search(self):
+        search_dialog = SearchDialog(self)
+        search_dialog.exec()
+
+    def edit(self):
+        edit_dialog = EditDialog(self)
+        edit_dialog.exec()
+
+    def delete(self):
+        delete_dialog = DeleteDialog(self)
+        delete_dialog.exec()
+
+    @staticmethod
+    def about():
+        about_dialog = AboutDialog()
+        about_dialog.exec()
 
 
 if __name__ == '__main__':
--- search_dialog.py
+++ search_dialog.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton, QComboBox, QLabel
 
 
 class SearchDialog(QDialog):
@@ -13,8 +13,8 @@ class SearchDialog(QDialog):
 
         # set window title and size
         self.setWindowTitle('Search Student')
-        self.setFixedWidth(200)
-        self.setFixedHeight(200)
+        self.setFixedWidth(300)
+        self.setFixedHeight(130)
 
         layout = QVBoxLayout()
 
@@ -22,18 +22,36 @@ class SearchDialog(QDialog):
         self.student_name = QLineEdit()
         self.student_name.setPlaceholderText('Name')
 
+        search_label = QLabel('Search By:')
+
+        self.search_by = QComboBox()
+        self.search_by.addItems(['Exactly', 'Contain'])
+        self.search_by.setToolTip('not case sensitive')
+
         button = QPushButton('Search')
         button.clicked.connect(self.search_student)
 
         # add widget to layout
         layout.addWidget(self.student_name)
+        layout.addWidget(search_label)
+        layout.addWidget(self.search_by)
         layout.addWidget(button)
 
         self.setLayout(layout)
 
     def search_student(self):
+
+        # clear selected table cells
         self.main_window.table.clearSelection()
+
         name = self.student_name.text()
-        items = self.main_window.table.findItems(name, Qt.MatchFlag.MatchContains)
+
+        # search by user reference
+        match self.search_by.currentText():
+            case 'Exactly':
+                items = self.main_window.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+            case 'Contain':
+                items = self.main_window.table.findItems(name, Qt.MatchFlag.MatchContains)
+
         for item in items:
             self.main_window.table.item(item.row(), item.column()).setSelected(True)
